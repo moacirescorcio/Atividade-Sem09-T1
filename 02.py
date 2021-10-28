a7b606e8bcf901a64e6439caea7b227f9769dd54
moacirescorcio <moacirescorcio@gmail.com>
2021-10-28 19:39:03 -0300

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,3 +1,14 @@
-n1 = int(input())
+n = 0
+i = 0
+impar = 0
+par = 0
 
-while n
+for i in range(5):
+    n = int(input())
+    if n % 2 == 0:
+        par + 1
+    else:
+        impar + 1
+
+print(par)
+print(impar)
