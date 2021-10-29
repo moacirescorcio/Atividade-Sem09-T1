07b3298e1d0cc359decb29595d444c382953ef3b
moacirescorcio <moacirescorcio@gmail.com>
2021-10-28 21:21:21 -0300

    5

---
+++ 03 - Em 100 números qual o valor médio.py
@@ -0,0 +1,7 @@
+n = 0
+
+for c in range (1, 101):
+    n += int(input())
+    
+media = n / 100
+print(f'{media:.2f}')
---
+++ 04 - Sequência.py
@@ -0,0 +1,4 @@
+for n in range (10, 991, 10):
+    print(n, end=", ")
+
+print("1000.")
